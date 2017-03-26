fa707ff79dbd9a1fc7b8db55d271ca46e5cf5a4b
YUNG-TA YEN <a867532@hotmail.com>
2017-03-27 02:57:50 +0800

    安裝套件、新增資料

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { D3Service, D3, Selection } from 'd3-ng2-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('container') container: ElementRef;
+  d3: D3;
+  svg: any;
+
+  constructor(private d3Service: D3Service){
+    this.d3 = d3Service.getD3();
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.svg = this.d3.select(this.container.nativeElement).append('svg')
+      .attrs({
+        width: 800,
+        height: 600
+      })
+      .styles({
+        'background-color': 'red'
+      });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { D3Service } from 'd3-ng2-service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [D3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
